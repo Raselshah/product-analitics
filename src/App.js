0b76e5afde7b630300e2945ad7a2be5626869fd9
Md Rasel Shah <mdraselshah56@gmail.com>
2022-04-04 13:15:10 +0600

    navbar home page added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import Reviews from "./Components/Reviews/Reviews";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Blogs from "./Components/Blogs/Blogs";
+import About from "./Components/About/About";
+import NotAPage from "./Components/NotAPage/NotAPage";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
-        <Route path="/" element={<Header></Header>} />
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/home" element={<Home></Home>} />
+        <Route path="/review" element={<Reviews></Reviews>} />
+        <Route path="/dashboard" element={<Dashboard></Dashboard>} />
+        <Route path="/blogs" element={<Blogs></Blogs>} />
+        <Route path="/about" element={<About></About>} />
+        <Route path="*" element={<NotAPage></NotAPage>} />
       </Routes>
     </div>
   );
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,9 +2,48 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h2>This is home</h2>
-    </div>
+    <>
+      <div className="grid justify-center items-center grid-cols-2">
+        <div className="">
+          <h2 className="text-orange-500 mb-5 text-5xl">
+            Hey are you food lover
+          </h2>
+          <h2 className="text-orange-300 text-5xl">
+            Please Order your favorite foods
+          </h2>
+          <p className="text-lg text-gray-500 mt-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint animi
+            impedit quis nam numquam quaerat distinctio libero at corrupti
+            eveniet non iste repellat itaque sed, assumenda officiis, commodi
+            ipsum nemo!
+          </p>
+          <button className="py-3 px-6 rounded bg-indigo-400 mt-5">
+            Love With Food
+          </button>
+        </div>
+        <div className="h-3/4 m-auto">
+          <img
+            className="h-[70vh] rounded"
+            src="https://images.pexels.com/photos/8104576/pexels-photo-8104576.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+        </div>
+      </div>
+
+      <h2 className="mt-20 mb-4 text-4xl">Customer Review</h2>
+      <div className="grid md:grid-cols-3">
+        <div className="card border-2 rounded">
+          <h2>head</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus veritatis rerum at quis ex ea quas minus illum
+            doloremque fugiat?
+          </p>
+          <p><small>Rating</small></p>
+          <button>Show more review</button>
+        </div>
+      </div>
+    </>
   );
 };
 
