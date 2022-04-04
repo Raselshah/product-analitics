b921097fb483fc8ec2971ad7a6eba1d26ed4a20a
Md Rasel Shah <mdraselshah56@gmail.com>
2022-04-04 17:49:53 +0600

    404 page design

--- src/Components/NotAPage/NotAPage.js
+++ src/Components/NotAPage/NotAPage.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const NotAPage = () => {
-    return (
-        <div>
-            <h2>Not 404</h2>
-        </div>
-    );
+  return (
+    <div className="h-[80vh]">
+      <div className="flex h-full justify-center items-center">
+        <img
+          src="https://community.librenms.org/uploads/default/original/2X/7/759793552edd033b80526884b06a706fdd1a06ba.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default NotAPage;
